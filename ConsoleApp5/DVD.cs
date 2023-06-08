79f4620d0d8081d90db33cd4bf30cabffda9e480
MichaelTs90909 <mic.tsyb@gmail.com>
2023-06-08 18:11:28 +0300

    Added DVD and HDD classes

--- ConsoleApp5/DVD.cs
+++ ConsoleApp5/DVD.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
-    public class DVD //: Storage
+    public class DVD : Storage
     {
+        public double Speed {get; private set;}
+        public override string GetInfo() => $"Name: {Name}\nModel: {Model}\nCapacity: {_capacity}\nFree memory: {_freeMemory}\nUsed memory: {_usedMemory}\nSpeed: {Speed}";
+        public DVD(string name, string model, double capacity, double speed) : base(name, model, capacity)
+        {
+
+        }
     }
 }
--- ConsoleApp5/HDD.cs
+++ ConsoleApp5/HDD.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp5
 {
-    public class HDD //: Storage
+    public class HDD : Storage
     {
+        public double Speed { get; set; }
+        public override string GetInfo() => $"Name: {Name}\nModel: {Model}\nCapacity: {_capacity}\nFree memory: {_freeMemory}\nUsed memory: {_usedMemory}\nSpeed: {Speed}";
+        public DVD(string name, string model, double capacity, double speed) : base(name, model, capacity)
+        {
+
+        }
     }
 }
