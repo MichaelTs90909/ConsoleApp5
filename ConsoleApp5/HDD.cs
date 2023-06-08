2b46d0a7d0414a2fc5df011695f4fe3a55044a00
stb.infoserv <stb.infoserv@gmail.com>
2023-06-08 18:30:32 +0300

    fixed HDD class

--- ConsoleApp5/HDD.cs
+++ ConsoleApp5/HDD.cs
@@ -8,11 +8,13 @@ namespace ConsoleApp5
 {
     public class HDD : Storage
     {
-        public double Speed { get; set; }
-        public override string GetInfo() => $"Name: {Name}\nModel: {Model}\nCapacity: {_capacity}\nFree memory: {_freeMemory}\nUsed memory: {_usedMemory}\nSpeed: {Speed}";
-        public DVD(string name, string model, double capacity, double speed) : base(name, model, capacity)
+        public double Speed { get; private set; }
+        public int Sectors { get; private set; }
+        public override string GetInfo() => $"Name: {Name}\nModel: {Model}\nCapacity: {_capacity}\nFree memory: {_freeMemory}\nUsed memory: {_usedMemory}\nSpeed: {Speed}\nSectors: {Sectors}";
+        public HDD(string name, string model, double capacity, double speed, int sectors) : base(name, model, capacity)
         {
-
+            Speed = speed < 2 ? 2 : speed;
+            Sectors = sectors > 0? sectors:100;
         }
     }
 }
