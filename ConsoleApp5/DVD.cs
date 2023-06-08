0173482db11902dcf55c22fc0cf0f572b9a51116
stb.infoserv <stb.infoserv@gmail.com>
2023-06-08 18:30:52 +0300

    fixed DVD class

--- ConsoleApp5/DVD.cs
+++ ConsoleApp5/DVD.cs
@@ -10,10 +10,13 @@ namespace ConsoleApp5
     public class DVD : Storage
     {
         public double Speed {get; private set;}
-        public override string GetInfo() => $"Name: {Name}\nModel: {Model}\nCapacity: {_capacity}\nFree memory: {_freeMemory}\nUsed memory: {_usedMemory}\nSpeed: {Speed}";
-        public DVD(string name, string model, double capacity, double speed) : base(name, model, capacity)
-        {
+        public int Sides { get; private set;}
 
+        public override string GetInfo() => $"Name: {Name}\nModel: {Model}\nCapacity: {_capacity}\nFree memory: {_freeMemory}\nUsed memory: {_usedMemory}\nSpeed: {Speed}\nSides: {Sides}";
+        public DVD(string name, string model, double capacity, double speed, int sides) : base(name, model, capacity) 
+        {
+            Speed = speed < 2 ? 2 : speed;
+            Sides = sides > 2? 2:1;
         }
     }
 }
